6150f094f4c385af85d58849b21cb7d4f4be1702
Robert G. Brown <rgb@phy.duke.edu>
2006-08-22 22:46:03 +0000

    This is definitely time to quit for the day, regardless of what is working or what isn't.

--- run_rgb_timing.c
+++ run_rgb_timing.c
@@ -21,13 +21,27 @@ void run_rgb_timing()
   * Declare the results struct.
   */
  Rgb_Timing timing;
+ Test **rgb_timing_test;
 
- printf("%s",rgb_timing_dtest.description);
+ /*
+  * First we create the test (to set some values displayed in test header
+  * correctly).
+  */
+ rgb_timing_test = create_test(&rgb_timing_dtest,tsamples,psamples,&rgb_timing);
+
+ /*
+  * Set any GLOBAL data used by the test.
+  */
+
+ /*
+  * Show the standard test header for this test.
+  */
+ show_test_header(&rgb_timing_dtest,rgb_timing_test);
 
  /*
   * Call the actual test that fills in the results struct.
   */
- rgb_timing(&timing);
+ rgb_timing(rgb_timing_test,&timing);
 
  /*
   * Display the results.
