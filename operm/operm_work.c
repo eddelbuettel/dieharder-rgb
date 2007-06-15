5425b6688a7e4cff6b9119c17766efa7654439f9
Robert G. Brown <rgb@phy.duke.edu>
2007-06-15 02:19:15 +0000

    This actually works for k = 2.  I think I need to add two loops to make it work for k = 3, which would be basically arbitrary k.

--- operm/operm_work.c
+++ operm/operm_work.c
@@ -17,12 +17,12 @@
 double f(int ip,gsl_permutation *ap);
 
 int k,kperms,n,nperms;
-gsl_permutation **fperms,**perms,*lperm,*rperm;
+gsl_permutation **fperms,**operms,**lperms,**rperms;
 
 void operm_work()
 {
 
- int i,j;
+ int i,j,l,m;
  double **c;
 
  /*
@@ -30,26 +30,12 @@ void operm_work()
   */
  k = 2;
 
- n = 2*k-1;
- nperms = gsl_sf_fact(n);
- perms = (gsl_permutation**) malloc(nperms*sizeof(gsl_permutation*));
- for(i=0;i<nperms;i++){
-   perms[i] = gsl_permutation_alloc(n);
-   if(i == 0){
-     gsl_permutation_init(perms[i]);
-   } else {
-     gsl_permutation_memcpy(perms[i],perms[i-1]);
-     gsl_permutation_next(perms[i]);
-   }
-   gsl_permutation_fprintf(stdout,perms[i],"%u ");
-   printf("\n");
- }
-
  /*
-  * We also need to form the permutations of 0 1 and 1 2 separately.
-  * They together form the "index" of f, e.g. f_0 selects 0 1 and f_1
-  * selects 1 0 (etc) in lexicographical order.  Note that we're
-  * shifting to C indices relative to the nilpotent markov chains paper.
+  * We first form the permutations of 0,1,...,k-1 in lexicographic order.
+  * This order determines the "index" argument to f, e.g. f(0,ap) determines
+  * if the permutation sent to it matches the one indexed by 0 or not and
+  * returns the centered statistic I_\pi - 1/k! either way.  This index
+  * is the index that matches the indices in the covariance matrix c[i][j].
   */
  kperms = gsl_sf_fact(k);
  fperms = (gsl_permutation**) malloc(kperms*sizeof(gsl_permutation*));
@@ -106,6 +92,69 @@ void operm_work()
   *   c[i][j] = (1/nperm) * sum_operm(f(i,lperm)*f(j,rperm))
   * (where yeah, this has to be turned into real code)
   */
+
+ /*
+  * First we make the overlapping permutations.  This part isn't
+  * quite right because when we get to k = 3 we can overlap:
+  *   0 1 2
+  *     1 2 3
+  *       2 3 4
+  * so we must loop over this, once we figure out the algorithm for
+  * k = 2 (which has a unique overlap).
+  */
+ n = 2*k-1;
+ nperms = gsl_sf_fact(n);
+ operms = (gsl_permutation**) malloc(nperms*sizeof(gsl_permutation*));
+ for(i=0;i<nperms;i++){
+   operms[i] = gsl_permutation_alloc(n);
+   if(i == 0){
+     gsl_permutation_init(operms[i]);
+   } else {
+     gsl_permutation_memcpy(operms[i],operms[i-1]);
+     gsl_permutation_next(operms[i]);
+   }
+   gsl_permutation_fprintf(stdout,operms[i],"%u ");
+   printf("\n");
+ }
+
+ /*
+  * Now sequentially sort them into left and right sides.
+  */
+ lperms = (gsl_permutation**) malloc(nperms*sizeof(gsl_permutation*));
+ rperms = (gsl_permutation**) malloc(nperms*sizeof(gsl_permutation*));
+ for(i=0;i<nperms;i++){
+   lperms[i] = gsl_permutation_alloc(k);
+   rperms[i] = gsl_permutation_alloc(k);
+   j = 0;
+   l = 0;
+   m = 0;
+   while(j<n){
+     if(operms[i]->data[j] < k) lperms[i]->data[l++] = operms[i]->data[j];
+     if(operms[i]->data[j] > 0) rperms[i]->data[m++] = operms[i]->data[j] - 1;
+     j++;
+   }
+   printf("lperms[%d] = ",i);
+   gsl_permutation_fprintf(stdout,lperms[i],"%u ");
+   printf("rperms[%d] = ",i);
+   gsl_permutation_fprintf(stdout,rperms[i],"%u ");
+   printf("\n");
+ }
+
+ printf("covariance = \n");
+ for(i=0;i<k;i++){
+   for(j=0;j<k;j++){
+     for(l=0;l<nperms;l++){
+       /* 
+       printf("f(%d,%d) = %f\n",i,l,f(i,lperms[l]));
+       printf("f(%d,%d) = %f\n",j,l,f(j,lperms[l]));
+       */
+       c[i][j] += f(i,lperms[l])*f(j,rperms[l]);
+     }
+     c[i][j] /= nperms;
+     printf("%8.4f  ",c[i][j]);
+   }
+   printf("\n");
+ }
   
 }
 
