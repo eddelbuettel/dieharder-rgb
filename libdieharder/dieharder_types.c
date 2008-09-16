1dd994b4d9a7a76d2b4016fece33df131e6d5442
Robert G. Brown <rgb@phy.duke.edu>
2008-09-16 21:09:16 +0000

    Once again, this is a transient checkin so that I can move and rearrange.  Things are actually coming together nicely (Dirk), and my remaining couple of chores are to make random number selection and test selection work much the same way, with a single three-digit index into a big vector of tests and a very simple name matcher/parser so that tests can be selected by number or by name.  I've built and tested the parser logic itself in parsecl.c, so it really should be no trouble to finish it, once I've got a bit of renaming out of the way.
    The separation of "dieharder" CLI operation and Rdh UI operation should
    be a piece of cake when I'm done -- a tiny handful of ifdefs, a single
    place you'll need "Rdh" code to set up the basic call in place of
    parsecl.c, and basically two commands that will be in your main
    interpreter loop -- one to pick and initialize an rng, one to invoke a
    test.  Output is all done in output.c, all dieharder output is in
    ifdefs, with a very few task-specific CLI pathways to e.g. print help or
    cause a list of rands to be dumped to stdout.  dieharder output is
    at this point completely controllable via a set of additive flags as we
    discussed -- you can truly roll your own output, trivially, including
    your own favorite field separator, while the default table-style output
    is still perfectly readable.
    
    I'm still holding off on a full checkin until I get this all finished,
    because it will very definitely need some serious testing and there
    isn't anything to test yet -- some things are done and work perfectly,
    but some things have yet to be done and are therefore "broken".
    
    Just FYI -- I was hoping to finish today, but might not make it unless I
    give up any hope of sleep.  I've got around 3-4 hours more work to the
    proper 3.28.0beta release.  More if you include fixing the documentation
    to match (but I may do that while you are playing with the beta).

--- libdieharder/dieharder_types.c
+++ libdieharder/dieharder_types.c
@@ -25,16 +25,15 @@
  *
  * While we're at it, let's define the ranges:
  *
- *   0-99 gsl generators (fixed order from now on)
- *   100-199 reserved for spillover if the gsl ends up with >100 generators
+ *   0-199 gsl generators (fixed order from now on with room for growth)
  *   200-399 libdieharder generators (fixed order from now on)
  *   400-499 R-based generators (fixed order from now on)
- *   500-799 reserved for future integration with R-like environments
- *   800-899 user-defined generators (starting with dieharder example)
- *   900-999 hardware generators (starting with /dev/random and friends)
+ *   500-599 hardware generators (e.g. /dev/random and friends)
+ *   600-699 user-defined generators (starting with dieharder example)
+ *   700-999 reserved for future integration with R-like environments
  *
- * Naturally, we can simply bump N below and add new ranges as needed, but
- * these will suffice for now.
+ * Naturally, we can simply bump MAXRNGS and add more, but 1000 seems
+ * likely to last for "a while" and maybe "forever".
  */
 
 #include <dieharder/libdieharder.h>
@@ -43,84 +42,122 @@ FILE *test_fp;
 const gsl_rng_type **dieharder_rng_types_setup(void)
 {
 
-  int i;
+ int i;
 
-  /*
-   * Null the whole thing for starters
-   */
-  for(i=0;i<MAXRNGS;i++) dieharder_types[i] = 0;
+ /*
+  * Null the whole thing for starters
+  */
+ for(i=0;i<MAXRNGS;i++) dh_types[i] = 0;
 
-  /*
-   * Initialize gsl_types to fill it with the current gsl rngs.
-   */
-  gsl_types = gsl_rng_types_setup();
+ /*
+  * Initialize gsl_types to fill it with the current gsl rngs.
+  */
+ gsl_types = gsl_rng_types_setup();
 
-  /*
-   * Copy its contents over into dieharder_rng_generator_types.
-   */
-  i = 0;
-  while(gsl_types[i] != NULL){
-    dieharder_types[i] = gsl_types[i];
-    i++;
-  }
+ /*
+  * Copy its contents over into dieharder_rng_generator_types.
+  */
+ i = 0;
+ while(gsl_types[i] != NULL){
+   dh_types[i] = gsl_types[i];
+   i++;
+ }
+ dh_num_gsl_rngs = i;
+ MYDEBUG(D_TYPES){
+   printf("# startup:  Found %u GSL rngs.\n",dh_num_gsl_rngs);
+ }
 
-  /*
-   * Now add the new ones in.  These positions are to be locked in by
-   * order within the ranges, so we need to be careful to get them
-   * "right" the first time.
-   *
-   * These are the dieharder generators.  I expect many users to use
-   * stdin-based raw input since it is by far the easiest one to come up
-   * with (and actually will work with e.g. /dev/random).  The file-based
-   * inputs will also be fairly common.  The rest are there for convenience,
-   * and to expose users to some new/interesting rngs.
-   */
-  i = 200;
-  ADD(gsl_rng_stdin_input_raw);
-  ADD(gsl_rng_file_input_raw);
-  ADD(gsl_rng_file_input);
-  ADD(gsl_rng_ca);
-  ADD(gsl_rng_uvag);
+ /*
+  * Now add the new ones in.  These positions are to be locked in by
+  * order within the ranges, so we need to be careful to get them
+  * "right" the first time.
+  *
+  * These are the dieharder generators.  I expect many users to use
+  * stdin-based raw input since it is by far the easiest one to come up
+  * with (and actually will work with e.g. /dev/random).  The file-based
+  * inputs will also be fairly common.  The rest are there for convenience,
+  * and to expose users to some new/interesting rngs.
+  */
+ i = 200;
+ dh_num_dieharder_rngs = 0; 
+ ADD(gsl_rng_stdin_input_raw);
+ dh_num_dieharder_rngs++;
+ ADD(gsl_rng_file_input_raw);
+ dh_num_dieharder_rngs++;
+ ADD(gsl_rng_file_input);
+ dh_num_dieharder_rngs++;
+ ADD(gsl_rng_ca);
+ dh_num_dieharder_rngs++;
+ ADD(gsl_rng_uvag);
+ dh_num_dieharder_rngs++;
+ MYDEBUG(D_TYPES){
+   printf("# startup:  Found %u dieharder rngs.\n",dh_num_dieharder_rngs);
+ }
 
-  /*
-   * These are the R-based generators.  Honestly it would be lovely
-   * to merge them with the GSL permanently.
-   */
-  i = 400;
-  ADD(gsl_rng_r_wichmann_hill);
-  ADD(gsl_rng_r_marsaglia_mc);
-  ADD(gsl_rng_r_super_duper);
-  ADD(gsl_rng_r_mersenne_twister);
-  ADD(gsl_rng_r_knuth_taocp);
-  ADD(gsl_rng_r_knuth_taocp2);
 
-  /*
-   * These are hardware/system generators.  Again, it would be lovely to
-   * merge them with the GSL permanently.  It would also be good to wrap
-   * these in conditionals so that they are added iff the hardware
-   * interface exists.  Perhaps we should try doing this -- it requires a
-   * call to stat, I believe.  But not now.
-   */
-  i = 500;
-  if (test_fp = fopen("/dev/random","r")) {
-    ADD(gsl_rng_dev_random);
-    fclose(test_fp);
-  }
-  if (test_fp = fopen("/dev/urandom","r")) {
-    ADD(gsl_rng_dev_urandom);
-    fclose(test_fp);
-  }
-  if (test_fp = fopen("/dev/arandom","r")) {
-    ADD(gsl_rng_dev_arandom);
-    fclose(test_fp);
-  }
+ /*
+  * These are the R-based generators.  Honestly it would be lovely
+  * to merge them with the GSL permanently.
+  */
+ i = 400;
+ dh_num_R_rngs = 0;
+ ADD(gsl_rng_r_wichmann_hill);
+ dh_num_R_rngs++;
+ ADD(gsl_rng_r_marsaglia_mc);
+ dh_num_R_rngs++;
+ ADD(gsl_rng_r_super_duper);
+ dh_num_R_rngs++;
+ ADD(gsl_rng_r_mersenne_twister);
+ dh_num_R_rngs++;
+ ADD(gsl_rng_r_knuth_taocp);
+ dh_num_R_rngs++;
+ ADD(gsl_rng_r_knuth_taocp2);
+ dh_num_R_rngs++;
+ MYDEBUG(D_TYPES){
+   printf("# startup:  Found %u R rngs.\n",dh_num_R_rngs);
+ }
 
-  /*
-   * This will let me change a single word in startup.c in dieharder
-   * and the program should "just work" with my custom generators and
-   * types in their new places.  We'll see, of course...
-   */
-  return dieharder_types;
+ /*
+  * These are hardware/system generators.  Again, it would be lovely to
+  * merge them with the GSL permanently.  It would also be good to wrap
+  * these in conditionals so that they are added iff the hardware
+  * interface exists.  Perhaps we should try doing this -- it requires a
+  * call to stat, I believe.  But not now.
+  */
+ i = 500;
+ dh_num_hardware_rngs = 0;
+ if (test_fp = fopen("/dev/random","r")) {
+   ADD(gsl_rng_dev_random);
+   fclose(test_fp);
+   dh_num_hardware_rngs++;
+ }
+ if (test_fp = fopen("/dev/urandom","r")) {
+   ADD(gsl_rng_dev_urandom);
+   fclose(test_fp);
+   dh_num_hardware_rngs++;
+ }
+ if (test_fp = fopen("/dev/arandom","r")) {
+   ADD(gsl_rng_dev_arandom);
+   fclose(test_fp);
+   dh_num_hardware_rngs++;
+ }
+ MYDEBUG(D_TYPES){
+   printf("# startup:  Found %u hardware rngs.\n",dh_num_hardware_rngs);
+ }
+
+ /*
+  * Tally up all the generators we found.
+  */
+ dh_num_rngs = dh_num_gsl_rngs + dh_num_dieharder_rngs + dh_num_R_rngs +
+            dh_num_hardware_rngs;
+
+ /*
+  * dh_types[] is "permanent" memory and doesn't have to be freed.
+  * This is possibly stupid; one "could" run dieharder_rng_types_setup()
+  * twice and THINK you are getting two distinct vectors of rngs, but
+  * you aren't.  OTOH, you won't leak memory...
+  */
+ return dh_types;
 
 }
 
