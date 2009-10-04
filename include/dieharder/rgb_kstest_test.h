9683429ed3587089dd3dac6ccd4d699fee8a821a
Robert G. Brown <rgb@phy.duke.edu>
2009-10-04 13:44:12 +0000

    Ignore today's checkins, Dirk.  I'm adding a new test (rgb_kstest_test) to test the kstest routines (as well as to MAYBE function as a new test in the suite, but I doubt that it will be sensitive enough to be any use).  Basically, I plan to fill a vector with tsamples uniform deviates, run a kstest on them (which tests for uniformity and generates a pvalue that should itself be a uniform deviate) to fill in the usual vector of pvalues and run the final kstest on that.  A kstest SHOULD recursively take uniform deviates to a uniform deviate, for a large enough set of uniform deviates, and I want to find out a) if this is true; and b) if it is true just what a "large enough set" is.  This test should help me find out both, and if a) is incorrect, to perhaps "fix" the kstest as this is their THEORETICAL behavior and failure to accomplish this indicates a bug in the code or a real problem in the theory...

---
+++ include/dieharder/rgb_kstest_test.h
@@ -0,0 +1,38 @@
+/*
+ * rgb_kstest_test test header
+ */
+
+/*
+ * function prototype
+ */
+int rgb_kstest_test(Test **test,int irun);
+
+static Dtest rgb_kstest_test_dtest = {
+  "RGB Kolmogorov-Smirnov Test Test",
+  "rgb_kstest_test",
+  "#\n\
+#            The Kolmogorov-Smirnov Test Test\n\
+#
+#\n\
+# This test generates a vector of tsamples uniform deviates from the\n\
+# selected rng, then applies an Anderson-Darling or Kuiper KS test to\n\
+# it to directly test for uniformity.  The AD version has been symmetrized\n\
+# to correct for weak left bias for small sample sets; Kuiper is already\n\
+# ring-symmetric on the interval.  The AD code corresponds roughly to\n\
+# what is in R (thanks to a correction sent in by David Bauer).\n\
+# As always, the test is run pvalues times and the (same) KS test is then\n\
+# used to generate a final test pvalue, but the real purpose of this test\n\
+# is to test ADKS and KKS, not to test rngs.  There is reason to believe\n\
+# that they are are returning high-biased final pvalues for the default\n\
+# 100 test pvalues in a standard test run, and this test will allow this\n\
+# hypothesis to be directly tested.  It may prove useful to detect\n\
+# extremely weak rngs as well (for large tsamples) but I somewhat doubt\n\
+# it.\n\
+#\n",
+  100,
+  100,
+  1,
+  rgb_kstest_test,
+  0
+};
+
